0a5ab8c8df48bd049b062cc69f32a7fd22e035bf
INDRA\ajmunoz <ajmunoz@minsait.com>
2022-06-15 12:33:23 +0200

    firs commit

---
+++ src/main/java/com/example/pruebas/openshift/example/pruebasopenshiftexample/PruebasopenshiftexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.pruebas.openshift.example.pruebasopenshiftexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebasopenshiftexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebasopenshiftexampleApplication.class, args);
+	}
+
+}
